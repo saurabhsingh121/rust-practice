ee210e2f52341c0a9a939998e3e9f2ba6f995fc0
saurabhsingh121 <saurabhsingh121@gmail.com>
2024-10-28 22:12:15 +0530

    enum declaration and variable
    Signed-off-by: saurabhsingh121 <saurabhsingh121@gmail.com>

--- media/src/main.rs
+++ media/src/main.rs
@@ -1,3 +1,19 @@
+
+#[derive(Debug)]
+enum Media {
+    Book{title: String, author:String},
+    Movie{title:String, director:String},
+    Audiobook{title:String},
+}
+
+fn print_media(media:Media){
+    println!("{:#?}", media)
+}
 fn main() {
-    println!("Hello, world!");
+   let auditobook = Media::Audiobook { title: String::from("An Audio Book") };
+   let good_movie = Media::Movie { title: String::from("A Good Movie"), director: String::from("Good Director") };
+   let book = Media::Book { title: String::from("Bad Book"), author: String::from("Bad Author") };
+   print_media(auditobook);
+   print_media(good_movie);
+   print_media(book);
 }
