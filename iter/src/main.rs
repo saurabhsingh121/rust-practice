d7c3a17e09f6941e9e2c25669b3a725ca956ad7d
saurabhsingh121 <saurabhsingh121@gmail.com>
2024-11-19 08:24:00 +0530

    shorten strings
    Signed-off-by: saurabhsingh121 <saurabhsingh121@gmail.com>

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -8,11 +8,17 @@ fn print_elements(elements: &[String]){
     .map(|el| format!("{} {}", el, el)) 
     .for_each(|el| println!("{}", el));
 }
+
+fn shorten_strings(elements: &mut Vec<String>){
+    elements.iter_mut().for_each(|el| el.truncate(1));
+}
 fn main() {
-    let colors = vec![
+    let mut colors = vec![
         String::from("red"),
         String::from("green"),
         String::from("blue")
     ];
+    // print_elements(&colors[1..3]);
+    shorten_strings(&mut colors);
     print_elements(&colors);
 }
