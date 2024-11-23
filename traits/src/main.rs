73550202d706a03daeafbd3e22062f922fea9f37
saurabhsingh121 <saurabhsingh121@gmail.com>
2024-11-23 13:57:29 +0530

    more on generics type
    Signed-off-by: saurabhsingh121 <saurabhsingh121@gmail.com>

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,19 @@
+mod basket;
+mod stack;
+
+use basket::Basket;
+use stack::Stack;
+
 fn main() {
-    println!("Hello, world!");
+    let b1 = Basket::new(String::from("hi there!"));
+
+    let b2 = Basket::new(10);
+
+    let b3 = Basket::new(true);
+
+    let s1 = Stack::new(
+        vec![String::from("hi")]
+    );
+
+    let s2 = Stack::new(vec![1,2,4]);
 }
