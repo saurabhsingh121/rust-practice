6c149dfb1ad57f7e61b572309da20021148e939f
saurabhsingh121 <saurabhsingh121@gmail.com>
2024-11-23 22:06:40 +0530

    traits
    Signed-off-by: saurabhsingh121 <saurabhsingh121@gmail.com>

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -2,19 +2,27 @@ mod basket;
 mod stack;
 mod container;
 
+use container::Container;
 use basket::Basket;
 use stack::Stack;
 
+fn add_string<T:Container<String>>(c:&mut T, s:String){
+    c.put(s);
+}
+
 fn main() {
-    let b1 = Basket::new(String::from("hi there!"));
+    let mut b1 = Basket::new(String::from("hi there!"));
 
     let b2 = Basket::new(10);
 
     let b3 = Basket::new(true);
 
-    let s1 = Stack::new(
+    let mut s1 = Stack::new(
         vec![String::from("hi")]
     );
 
     let s2 = Stack::new(vec![1,2,4]);
+
+    add_string(&mut b1, String::from("hi"));
+    add_string(&mut s1, String::from("hi"));
 }
